9f62020506218c4ec55f1f02d19d7cfb1e4b3916
Freddie Lumor <freddielumor@gmail.com>
2020-09-27 14:47:16 +0100

    Update Packages
    - Update webpack loaders
    - Add new packages

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,21 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[hash]-[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|gif)$/,
+        use: 'file-loader?name=fonts/[name].[ext]!static',
+      },
     ],
   },
   resolve: {
