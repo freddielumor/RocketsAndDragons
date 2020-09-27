aed5fbe8d7f16c415489b37c60d6a831f2dbc984
Freddie Lumor <freddielumor@gmail.com>
2020-09-27 14:53:01 +0100

    Update App Setup
    - Add Router to index file
    - Update App.tsx with routing and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
 import * as React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { Router } from '@reach/router'
-import { store } from './store'
-import Header from './components/Header/Header'
+import { store } from './redux/store'
+import { AppHeader } from './components/Header/Header'
+import { AppNav } from './components/Nav/Nav'
 import Rockets from './components/Rockets/Rockets'
 import Dragons from './components/Dragons/Dragons'
+import RocketItem from './components/RocketItem/RocketItem'
+import './styles/base/index.scss'
 
 interface IAppProps {}
 
 const App: React.FC<IAppProps> = () => (
   <Provider store={store}>
     <div className="app">
-      <Header />
+      <AppHeader>
+        <AppNav />
+      </AppHeader>
 
-      <Router>
-        <Rockets path="/" />
-        <Dragons path="/dragons" />
-      </Router>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Rockets />
+          </Route>
+
+          <Route path="/dragons">
+            <Dragons />
+          </Route>
+
+          <Route path="/rockets/:rocket_id">
+            <RocketItem />
+          </Route>
+        </Switch>
+      </main>
     </div>
   </Provider>
 )
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./App";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root'),
+)
