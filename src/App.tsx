35f386cdd7e85b7418c24c154e698dfa02d839ed
Freddie Lumor <freddielumor@gmail.com>
2020-09-27 20:44:14 +0100

    Add Not Found Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
+
 import { AppHeader } from './components/Header/Header'
 import { AppNav } from './components/Nav/Nav'
+
 import Rockets from './components/Rockets/Rockets'
 import Dragons from './components/Dragons/Dragons'
 import RocketItem from './components/RocketItem/RocketItem'
+import PageNotFound from './components/PageNotFound/PageNotFound'
 import './styles/base/index.scss'
 
 interface IAppProps {}
@@ -30,6 +33,10 @@ const App: React.FC<IAppProps> = () => (
 
           <Route path="/rockets/:rocket_id">
             <RocketItem />
+          </Route>
+
+          <Route path="*" exact>
+            <PageNotFound />
           </Route>
         </Switch>
       </main>
