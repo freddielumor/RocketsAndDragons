487d4389b19f35f559a71faf28075794a8093964
Freddie Lumor <freddielumor@gmail.com>
2020-09-30 00:21:13 +0100

    Cleanup

--- src/utils/testingUtils.js
+++ src/utils/testingUtils.js
@@ -10,13 +10,15 @@ import '@testing-library/jest-dom'
 // TESTING UTILITY
 
 // This util alllows us to replicate the application state for testing purposes.
-// We export a render function which contains the application state, to allow us to test our components in isolation their integration across the app.
-
 // 1. Setup initial state and mock store
+// 2. Create a test provider containing the state & router
+// 3. Create the test render function which is used in our test files
+// 4. re-export everything from Testing Library
+// 5. Override render method
+
 const initialState = {}
 const mockStore = createStore(rootReducer, initialState, applyMiddleware(thunk))
 
-// 2. Create a test provider containing the state & router
 const testWrapper = ({ children }) => {
   return (
     <Router>
@@ -25,11 +27,8 @@ const testWrapper = ({ children }) => {
   )
 }
 
-// 3. Create the test render function which is used in our test files
 const testRender = (ui) => render(ui, { wrapper: testWrapper })
 
-// 4. re-export everything from Testing Library
 export * from '@testing-library/react'
 
-// 5. Override render method
 export { testRender as render }
